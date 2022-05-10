34cbfe91f5a8ee221cc4dc1bca2ad8a747e03597
Guilherme <99288564+Guilherme4444@users.noreply.github.com>
2022-05-09 22:23:23 -0300

    Add files via upload

---
+++ ExemploMath1.java
@@ -0,0 +1,7 @@
+public class ExemploMath1 {
+    public static void main(String[] args) {
+      System.out.println(Math.abs(-4.7));  
+      System.out.println(Math.abs(4.7));  
+      System.out.println(Math.abs(3));  
+    }
+  }
