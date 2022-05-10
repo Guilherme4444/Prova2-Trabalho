efe0a4f20bb3d5b15810ea2fa539237de937ac2b
Guilherme <99288564+Guilherme4444@users.noreply.github.com>
2022-05-09 21:20:29 -0300

    Add files via upload

---
+++ ExemploString.java
@@ -0,0 +1,12 @@
+public class ExemploString {
+
+    public static void main(String[] args) {
+        
+        String java = "java";
+        for (int i=0; i<java.length(); i++){
+            System.out.println(java.charAt(i));
+        }
+
+        
+    }
+}
